0888cda9455fc98acb7b572c51d724fd8052dcdc
banna11 <41046192+banna11@users.noreply.github.com>
2019-10-29 01:07:04 +0100

    Add files via upload

---
+++ main_code.R
@@ -0,0 +1,63 @@
+library(readxl)
+library(schoolmath)
+library(lineup)
+library(dplyr)
+library(ggplot2)
+library(zoo)
+
+setwd("C:/Users/banna/Documents/egyetem/EFRP/R/efrp_r_reizinger-master/efrp_r_reizinger-master")
+
+#0.) Loading functions
+source("korrel.R")
+source("param.R")
+source("read.R")
+source("all_correlations.R")
+
+
+source("select.R")
+
+# data
+#1.)Set wd
+dataset <- c("C:/Users/banna/Documents/egyetem/EFRP/R/efrp_r_reizinger-master/efrp_r_reizinger-master/WTI2.xlsx")
+
+#2.) Input for reading data
+number_of_products <- 3 # 24 # number of analysed products
+assertthat::validate_that(is.numeric(number_of_products)==T)
+
+last_col_name <- Excel_colnames_find()[number_of_products + 1] # first col is Date
+last_row_number <- 2558 # number of rows
+read_range <- paste0("A1:",last_col_name,last_row_number) # converts inputs for Excel format
+
+#3.) 
+ #a.) param() stores the data
+  # You should add your input parameters (windowsize and correlation lag) opening opening() function
+ #b.) korrel() calculate only 1 correlation
+ #c.)select.param() select one of the possible window size and correlation lag by adding the indexes
+  # you can rewrite the possibilities in param() function
+  #Window Size
+    #view the possibilities
+    param()[["WindowSize"]]
+    #choose the index
+    i1 <-1
+  #Correlation Lag
+    #view the possibilities
+    param()[["CorrelLag"]]
+    #choose the index
+    i2 <-1
+
+ #d.) all_correlations() output is an xts format table (rows=Date, cols=correlation pairs)
+
+head(all_correlations())
+
+ 
+ 
+
+# 4.) Plot cross-correlations
+correl <- all_correlations() #dataset for plotting
+days <-  as.Date(seq(from = as.Date("2010-02-20"), to = as.Date("2016-12-31"), by = 'day'))
+plot <- ggplot(data=correl, aes(as.Date(days))) +
+         geom_line(aes( y = name1, colour ="blue")) +
+         geom_line(aes( y = name2, colour="red")) +
+         geom_line(aes( y = name3, colour="green")) +
+         ggtitle("Keresztkorreáció") + xlab("Dátum") + ylab("Érték")
+plot
